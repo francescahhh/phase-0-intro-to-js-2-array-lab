ad3dcd4cbc4f6c3ffd0f915634425aad9ee028c2
Francesca Hamm <francesca.c.hamm@gmail.com>
2021-11-06 18:20:39 -0700

    commit tree 12492e9c572630e61ec7cd984400163722ec332d

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(catName) {
+    return cats.push(catName); 
+}
+
+function destructivelyPrependCat(catName) {
+    return cats.unshift(catName); 
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(catName) {
+    let newCats = [...cats, catName];
+    return newCats;
+}
+
+function prependCat(catName) {
+    let newCats = [catName, ...cats];
+    return newCats;
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
